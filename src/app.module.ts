fd9919d8dfad1dee571ae94a05ccd97f5f65fbf2
callmeaquitter <anastasialomejor@gmail.com>
2024-02-06 12:32:55 +0300

    deleted and created new files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
+import { PromoModule } from './promo/promo.module';
 
 
 @Module({
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    PromoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
