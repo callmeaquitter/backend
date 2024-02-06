103b799e2e5ee3a76226fd58b9f13fa83fa0cb5c
callmeaquitter <anastasialomejor@gmail.com>
2024-02-06 10:14:40 +0300

    env + config ++ init

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const port = parseInt(process.env.PORT);
+  console.log('prot = ', process.env.PORT); //отладка
+  const server = process.env.SERVER;
+  await app.listen(port, server);
+
+  console.log('Application is running on: ${await app.getUrl()}');
+}
+bootstrap();
