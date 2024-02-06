6b428daa2da37aebcd79150c3b019d8a60b1df79
callmeaquitter <anastasialomejor@gmail.com>
2024-02-06 12:33:51 +0300

    deleted appservice and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,5 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 import { ConfigModule } from '@nestjs/config';
-import { PromoModule } from './promo/promo.module';
 
 
 @Module({
@@ -11,11 +7,6 @@ import { PromoModule } from './promo/promo.module';
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    PromoModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
-
-// commit: env + config  ++ init
