de0f39c7936261ca2f24b02bf92a948ad144d590
callmeaquitter <anastasialomejor@gmail.com>
2024-02-07 11:06:45 +0300

    things i didnt do yesterday

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getPostgresConfig } from './configs/postgres.config';
+import { PromoModule } from './promo/promo.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getPostgresConfig,
+    }),
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
